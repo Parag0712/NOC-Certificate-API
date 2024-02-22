95d538fb209fbf4abda9f68146c427f26d0eb15e
Parag0712 <paragvadgama123@gmail.com>
2024-02-22 12:34:20 +0530

    db.js added

---
+++ db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDb = async()=>{
+    try {
+        
+    const connectionInstance = mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED:", error);
+        process.exit(1)//FATAL ERROR (Process is a object of node )
+    }
+}
+
+export default connectDb;
